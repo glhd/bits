c05eb8e816d5a3caf30fa966cada499adf75663e
Samuel Levy <sam@determineddevelopment.com>
2024-11-26 15:09:34 +1000

    Added failing test for automatic casting
    Included tests for the newer Laravel 11 `casts()` method

--- tests/Feature/SonyflakeCastTest.php
+++ tests/Feature/SonyflakeCastTest.php
@@ -3,36 +3,37 @@
 namespace Glhd\Bits\Tests\Feature;
 
 use Glhd\Bits\Database\HasSnowflakes;
-use Glhd\Bits\Snowflake;
+use Glhd\Bits\Sonyflake;
 use Glhd\Bits\Tests\ResolvesSequencesFromMemory;
 use Glhd\Bits\Tests\TestCase;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Schema;
+use Orchestra\Testbench\Attributes\DefineEnvironment;
 use PHPUnit\Framework\Attributes\DataProvider;
 
-class SnowflakeCastTest extends TestCase
+class SonyflakeCastTest extends TestCase
 {
 	use ResolvesSequencesFromMemory;
-	
+
 	protected function setUp(): void
 	{
 		parent::setUp();
 		
-		Schema::create('test_models', function(Blueprint $table) {
+		Schema::create('test_sonyflake_models', function(Blueprint $table) {
 			$table->unsignedBigInteger('id')->primary();
 			$table->string('label')->nullable();
 			$table->timestamps();
 		});
 
-		Schema::create('test_casts_method_models', function(Blueprint $table) {
+		Schema::create('test_sonyflake_casts_method_models', function(Blueprint $table) {
 			$table->unsignedBigInteger('id')->primary();
 			$table->string('label')->nullable();
 			$table->timestamps();
 		});
 
-		Schema::create('test_automatic_casts_models', function(Blueprint $table) {
+		Schema::create('test_sonyflake_automatic_casts_models', function(Blueprint $table) {
 			$table->unsignedBigInteger('id')->primary();
 			$table->string('label')->nullable();
 			$table->timestamps();
@@ -41,19 +42,21 @@ class SnowflakeCastTest extends TestCase
 
 	/** @param class-string<Model> $class */
 	#[DataProvider('modelClassProvider')]
+	#[DefineEnvironment('usesSonyflakes')]
 	public function test_it_casts_attributes(string $class): void
 	{
 		$model1 = $class::create();
 		$model2 = $class::create();
 		
-		$this->assertInstanceOf(Snowflake::class, $model1->id);
-		$this->assertInstanceOf(Snowflake::class, $model2->id);
+		$this->assertInstanceOf(Sonyflake::class, $model1->id);
+		$this->assertInstanceOf(Sonyflake::class, $model2->id);
 		
 		$this->assertTrue($model2->id->id() > $model1->id->id());
 	}
 
 	/** @param class-string<Model> $class */
 	#[DataProvider('modelClassProvider')]
+	#[DefineEnvironment('usesSonyflakes')]
 	public function test_you_can_set_id_manually(string $class): void
 	{
 		$model = $class::forceCreate(['id' => 123]);
@@ -71,36 +74,36 @@ class SnowflakeCastTest extends TestCase
 	{
 		$with_casts_method = (int) Application::VERSION >= 11;
 
-		return array_filter([
-			'casts property' => [TestModel::class],
-			'casts method' => $with_casts_method ? [TestCastsMethodModel::class] : null,
-			'automatic casting' => [TestAutomaticCastsModel::class],
-		]);
+		return [
+			'casts property' => [TestSonyflakeModel::class],
+			'casts method' => $with_casts_method ? [TestSonyflakeCastsMethodModel::class] : null,
+			'automatic casting' => [TestSonyflakeAutomaticCastsModel::class],
+		];
 	}
 }
 
-class TestModel extends Model
+class TestSonyflakeModel extends Model
 {
 	use HasSnowflakes;
-	
+
 	protected $casts = [
-		'id' => Snowflake::class,
+		'id' => Sonyflake::class,
 	];
 }
 
-class TestCastsMethodModel extends Model
+class TestSonyflakeCastsMethodModel extends Model
 {
 	use HasSnowflakes;
 
 	protected function casts()
 	{
 		return [
-			'id' => Snowflake::class,
+			'id' => Sonyflake::class,
 		];
 	}
 }
 
-class TestAutomaticCastsModel extends Model
+class TestSonyflakeAutomaticCastsModel extends Model
 {
 	use HasSnowflakes;
 }
