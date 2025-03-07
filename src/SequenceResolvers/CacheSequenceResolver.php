44693a1b19564d74169adb04242a67a20fe39e4c
Chris Morrell <inxilpro@users.noreply.github.com>
2025-03-07 09:29:34 -0500

    Mess with indent

--- src/SequenceResolvers/CacheSequenceResolver.php
+++ src/SequenceResolvers/CacheSequenceResolver.php
@@ -13,40 +13,40 @@ class CacheSequenceResolver implements ResolvesSequences
 		protected Repository $cache,
 	) {
 	}
-
+	
 	public function next(int $timestamp): int
 	{
 		$key = "glhd-bits-seq:{$timestamp}";
-
+		
 		if ($this->cache->getStore() instanceof RedisStore) {
 			return $this->redisSafeIncrement($key, ttl: 10);
 		}
-
+		
 		$this->cache->add($key, 0, now()->addSeconds(10));
-
+		
 		return $this->cache->increment($key) - 1;
 	}
-
+	
 	protected function redisSafeIncrement(string $key, int $ttl): int
 	{
 		$store = $this->cache->getStore();
 		$key = $store->getPrefix().$key;
-
+		
 		$script = <<<LUA
-            local ttl = tonumber(ARGV[1])
-            local value = redis.call('INCR', KEYS[1])
-            if value == 1 then
-                redis.call('EXPIRE', KEYS[1], ttl)
-            end
-            return value - 1
-        LUA;
-
+		local ttl = tonumber(ARGV[1])
+		local value = redis.call('INCR', KEYS[1])
+		if value == 1 then
+		    redis.call('EXPIRE', KEYS[1], ttl)
+		end
+		return value - 1
+		LUA;
+		
 		$connection = $store->connection();
-
+		
 		if ($connection instanceof PredisConnection) {
 			return (int) $connection->client()->eval($script, 1, $key, $ttl);
 		}
-
+		
 		return (int) $connection->client()->eval($script, [$key, $ttl], 1);
 	}
 }
