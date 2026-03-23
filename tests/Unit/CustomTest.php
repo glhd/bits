981bcea991c1ed5ccc2242fb68ffd08e608b11a3
Michael Dyrynda <michael@dyrynda.com.au>
2026-03-24 01:57:50 +1030

    Add Laravel 13 support (#18)
    * add laravel 13 support
    
    * Fix whitespace
    
    ---------
    
    Co-authored-by: Chris Morrell <inxilpro@users.noreply.github.com>

--- tests/Unit/CustomTest.php
+++ tests/Unit/CustomTest.php
@@ -219,7 +219,7 @@ class CustomTest extends TestCase
 		$this->assertEquals([42, 0, 0], $bits->values->all());
 	}
 	
-	protected function getBitsFactory(int $id, ResolvesSequences $sequence = null): GenericFactory
+	protected function getBitsFactory(int $id, ?ResolvesSequences $sequence = null): GenericFactory
 	{
 		$config = new GenericConfig(
 			precision: 6,
